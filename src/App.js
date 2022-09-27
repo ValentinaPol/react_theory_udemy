736e86208b6297438eb051524344f41d91448540
ValentinaPol <polianskaya.valentina@gmail.com>
2022-09-27 13:38:42 +0300

    Context API

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Car from './Car/Car'
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 import Counter from './Counter/Counter'
 
+export const ClickedContext = React.createContext(false)
+
 class App extends Component {
 
   constructor(props) {
@@ -79,9 +81,10 @@ class App extends Component {
       <div style={divStyle}>
         {/* <h1>{this.state.pageTitle}</h1> */}
         <h1>{this.props.title}</h1>
-
-        <Counter clicked={this.state.clicked}/>
-
+        <ClickedContext.Provider value={this.state.clicked}>
+          <Counter />
+        </ClickedContext.Provider>
+        
         <hr/>
         <button 
           style={{marginTop: 20}}
